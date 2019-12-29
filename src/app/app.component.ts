63903c79d02fc4ebeefbb7e7254bfca477e4c96a
Rajendar Gade <rajendar.gade@cnet-global.com>
2019-12-29 17:01:23 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Person {
+  name: string
+  blogurl: string
+  twitter: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'clr-test-prj';
+  
+  people: Person[] = [
+    {name: 'Tutsplus blog', blogurl: 'https://code.tutsplus.com/categories/angular', twitter: "@"},
+    {name: 'The New!', blogurl: 'https://www.tothenew.com/blog/category/technology/angular/', twitter: "@"},
+    {name: 'Juri', blogurl: 'https://juristr.com/page/speaking/', twitter: "@"},
+    {name: 'Dan', blogurl: 'https://blog.codewithdan.com/', twitter: "@"},
+    {name: 'Adrain Meja', blogurl: 'https://adrianmejia.com/', twitter: "@"},
+    {name: 'Jason watmore', blogurl: 'https://jasonwatmore.com/', twitter: "@"}
+  ]
 }
